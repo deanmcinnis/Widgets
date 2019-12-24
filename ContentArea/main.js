8227b6b59215da804dede4d28d166c08db7ede20
deanmcinnis <deansemailbox@yahoo.com>
2019-12-24 08:29:19 -0500

    changing back

--- ContentArea/main.js
+++ ContentArea/main.js
@@ -1,23 +1,32 @@
 
+var x = location.origin;
+console.log(x);
+
 
     var myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
-    myRequest.onload = function() {
+   // myRequest.open('GET', 'https://learnwebcode.github.io/json-example/pets-data.json');
+   myRequest.open('GET', 'C:\Users\dean\Desktop\go-workspace\src\Widgets\ContentArea\testdata.json'); 
+   myRequest.responseType = 'json' ;
+  
+   
+   myRequest.onload = function() {
         if (myRequest.status >= 200 && myRequest.status < 400) {
             var myData = JSON.parse(myRequest.responseText);
             createHTML(myData);
         } else {
             console.log("We connected to the server but had an error");
+
         }
     };
     
     myRequest.onerror = function() {
         console.log("Connection error");
+        alert(`Error ${myRequest.status}: ${myRequest.statusText}`);
     }
     myRequest.send();
    
 function createHTML(Data) {
-    var rawTemplate = document.getElementById("petsTemplate").innerHTML;
+    var rawTemplate = document.getElementById("questionsTemplate").innerHTML;
     var compliledTemplate = Handlebars.compile(rawTemplate);
     var ourGeneratedHTML = compliledTemplate(Data);
 
